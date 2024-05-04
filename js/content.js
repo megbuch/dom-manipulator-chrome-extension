29d9866b0b7eff0bf993d476ef5a9f33b5d55cff
Meghan Bucher <115611931+megbuch@users.noreply.github.com>
2024-05-03 23:13:30 -0700

    Create basic structure, styling, and add dependencies.

--- js/content.js
+++ js/content.js
@@ -1,1 +1,20 @@
 // Handles interaction with the web page
+
+let selectedElement
+let editMode = false
+
+const selectElement = event => {
+  event.preventDefault()
+  if (!editMode) return
+  
+  selectedElement = event.target
+  console.log(selectedElement)
+}
+
+const toggleEditMode = request => {
+  if (request.toggle === "editMode") editMode = !editMode
+  console.log('edit mode toggled')
+}
+
+document.addEventListener('click', selectElement)
+chrome.runtime.onMessage.addListener(toggleEditMode)
--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,12 @@
 // Handles interaction in extension window
+
+const toggleEditModeButton = document.getElementById('toggle-edit-mode-btn')
+
+const sendToggleEditModeMessage = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs.length === 0) return
+    chrome.tabs.sendMessage(tabs[0].id, {toggle: "editMode"})
+  });
+}
+
+toggleEditModeButton.addEventListener('click', sendToggleEditModeMessage)
