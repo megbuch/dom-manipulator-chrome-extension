4b19b732ddd3a11cce7f9c0b5941539bdf94b10d
Melanie Winter <melaniebucher830@gmail.com>
2024-05-04 23:18:23 -0700

    Changes based on feedback

--- js/content.js
+++ js/content.js
@@ -71,41 +71,43 @@ const toggleEditMode = request => {
   }
 }
 
-dragElement(document.getElementById("floating-modal"))
+const startDragModal = element => {
+  let pos1 = 0
+  let pos2 = 0
+  let pos3 = 0
+  let pos4 = 0
 
-function dragElement(element) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
+  const dragMouseDown = event => {
+    event.preventDefault()
+    pos3 = event.clientX
+    pos4 = event.clientY
+    document.onmouseup = stopDragModal
+    document.onmousemove = dragModal
+  }
+
+  const dragModal = event => {
+    event.preventDefault()
+    pos1 = pos3 - event.clientX
+    pos2 = pos4 - event.clientY
+    pos3 = event.clientX
+    pos4 = event.clientY
+    element.style.top = (element.offsetTop - pos2) + "px"
+    element.style.left = (element.offsetLeft - pos1) + "px"
+  }
+
+  const stopDragModal = () => {
+    document.onmouseup = null
+    document.onmousemove = null
+  }
+
   if (document.getElementById("modal-content")) {
     document.getElementById("modal-content").onmousedown = dragMouseDown
   } else {
     element.onmousedown = dragMouseDown
   }
-
-  function dragMouseDown(e) {
-    e = e || window.event
-    e.preventDefault()
-    pos3 = e.clientX
-    pos4 = e.clientY
-    document.onmouseup = closeDragElement
-    document.onmousemove = elementDrag
-  }
-
-  function elementDrag(e) {
-    e = e || window.event
-    e.preventDefault()
-    pos1 = pos3 - e.clientX
-    pos2 = pos4 - e.clientY
-    pos3 = e.clientX
-    pos4 = e.clientY
-    element.style.top = (element.offsetTop - pos2) + "px"
-    element.style.left = (element.offsetLeft - pos1) + "px"
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null
-    document.onmousemove = null
-  }
 }
+
+startDragModal(document.getElementById("floating-modal"))
 
 window.addEventListener('click', selectElement)
 chrome.runtime.onMessage.addListener(toggleEditMode)
